66bd5739dd8c7e31f31abe5ca02fca40d044a253
Augustine Kim <augustine.kim1104@gmail.com>
2019-02-25 23:23:27 -0700

    Initial version. Only documented descriptions are added.

---
+++ AugustineKim_30399_Assignment2_Calculator/src/cse360assign2/Calculator.java
@@ -0,0 +1,84 @@
+package cse360assign2;
+
+/**
+ * Calculator computes simple algebraic operations in the order as called.
+ * The total is initiated to zero when a Calculator object is made, and the
+ * input operations are all assumed to be an integer. The current value of
+ * the total can be displayed along with the history of what operations were
+ * called with the corresponding values.
+ * 
+ * @author Augustine Kim
+ */
+public class Calculator
+{
+	//private int total;
+	
+	/**
+	 * Class constructor
+	 */
+	public Calculator () 
+	{
+	//	total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the value of the current total.
+	 * 
+	 * @return			Current total
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**
+	 * Adds the total by the input value.
+	 * 
+	 * @param value		
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Subtracts the total by the input value.
+	 * 
+	 * @param value 	The value being subtracted from the total
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Multiplies the total by the input value.
+	 * 
+	 * @param value		The value being multiplied by the total
+	 */
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Divides the total by the input value. If the value is zero,
+	 * the total becomes zero instead. No errors/exceptions.
+	 * 
+	 * @param value		The value being divided by the total
+	 */
+	public void divide (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Displays an the 
+	 * 
+	 * @return			Empty output.
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
